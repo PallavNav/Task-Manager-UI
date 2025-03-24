a8c384304833a58ecec65d81bc841d1f6035c499
Nav Pallav <pallavnav@gmail.com>
2025-03-24 23:42:04 +0530

    With dynamic updating the urk

--- src/services/service.tsx
+++ src/services/service.tsx
@@ -1,5 +1,3 @@
 export const apiServices = {
-    base_url: import.meta.env.VITE_API_BASE_URL
-        ? `${import.meta.env.VITE_API_BASE_URL}/nav/taskify`
-        : 'http://localhost:9000/nav/taskify',
+    base_url: import.meta.env.VITE_API_URL
 };
