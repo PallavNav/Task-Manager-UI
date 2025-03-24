3e0cacc1004ba55e18f40a52c269024b5e5e1d91
Nav Pallav <pallavnav@gmail.com>
2025-03-25 00:25:09 +0530

    With dynamic updating the urk

--- src/services/service.tsx
+++ src/services/service.tsx
@@ -1,7 +1,7 @@
 export const apiServices = {
     get base_url() {
-        return import.meta.env.VITE_API_URL || "http://localhost:9000"; 
+      return import.meta.env.VITE_API_URL;
     },
     test: "Nav"
-};
-
+  };
+  
