f911b51f19e7ef25251205387d979ea9556628ea
Nav Pallav <pallavnav@gmail.com>
2025-04-01 00:53:00 +0530

    Header update

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "../styles/TaskList.css";
 import { ConvertDate } from "../util/ConvertDate";
 import { useState, useEffect } from "react";
+import { apiServices } from "../services/service";
 
 type TaskListProps = {
   tasks: Task[];
@@ -25,7 +26,7 @@ const TaskList = ({ tasks, onDelete, checkBoxSelection, handleSelectAll }: TaskL
   const [sortColumn, setSortColumn] = useState<any>('dueDate');
   const [isAllChecked, setIsAllChecked] = useState<boolean>(false);
   const truncationLimit = 25;
-
+  const IS_LOCAL = apiServices.IS_LOCAL;
   useEffect(() => {
     handleSort("dueDate", "asc");
   }, []);
@@ -71,7 +72,7 @@ const TaskList = ({ tasks, onDelete, checkBoxSelection, handleSelectAll }: TaskL
           <table className="task-table">
             <thead>
               <tr>
-                <th><input type="checkbox" checked={isAllChecked} onClick={handleSelectAll}/>Select All </th>
+                {IS_LOCAL && <th><input type="checkbox" checked={isAllChecked} onClick={handleSelectAll}/>Select All </th>}
                 <th className="field-label cursor sortable" onClick={() => handleSort("title", undefined)}>Title {getSortIndicator("title")}</th>
                 <th className="field-label cursor sortable" onClick={() => handleSort("description", undefined)}>Description {getSortIndicator("description")}</th>
                 <th className="field-label cursor sortable" onClick={() => handleSort("dueDate", undefined)}>Due Date {getSortIndicator("dueDate")}</th>
@@ -83,7 +84,7 @@ const TaskList = ({ tasks, onDelete, checkBoxSelection, handleSelectAll }: TaskL
             <tbody>
               {tasks.length > 0 && tasks.map((task) => (
                 <tr key={task.id}>
-                  <td><input type="checkbox" checked={task.isChecked} onClick={(e: any) => checkBoxSelection(task.id, e.target.checked)} /></td>
+                  {IS_LOCAL && <td><input type="checkbox" checked={task.isChecked} onClick={(e: any) => checkBoxSelection(task.id, e.target.checked)} /></td>}
                   <td title={task.title}>{(task.title.length > truncationLimit) ? task.title.slice(0, truncationLimit).concat('...') : task.title}</td>
                   <td title={task.description}>{(task.description.length > truncationLimit) ? task.description.slice(0, truncationLimit).concat('...') : task.description}</td>
                   <td title={ConvertDate(task.dueDate)}>{ConvertDate(task.dueDate)}</td>
